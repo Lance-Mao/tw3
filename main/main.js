4261bc479d93e89226a2e3143c95c463d9a3f797
M977986310 <977986310@qq.com>
2017-05-21 10:31:46 +0800

    three

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 module.exports = function main(str) {
     var sum = 0;
     var array = Array.from(str);
-    return array.reduce((a, b) => a + b);
+    return array.reduce((a, b) => parseInt(a )+ parseInt(b));
 };
